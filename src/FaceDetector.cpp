81bcb285c58606499aff825deaa212fddee89582
Satvik Tandon <satviktandon@gmail.com>
2022-12-01 14:33:11 +0530

    Update FaceDetector.cpp

--- src/FaceDetector.cpp
+++ src/FaceDetector.cpp
@@ -7,9 +7,15 @@ using namespace std;
 using namespace dnn;
 using namespace cv;
 
+/*readNetFromCaffe takes two files to construct the model:
+1. The first (deploy.prototxt) is the model configuration which describes the model archtecture.
+2. The second (res10_300x300_ssd_iter_140000_fp16.caffemodel) is the binary data for the model weights.*/
+
+
+//defining FaceDetector function and initializing the parameters
 FaceDetector::FaceDetector() : confidence_threshold_(0.5), input_image_height_(300), input_image_width_(300), scale_factor_(1.0), mean_values_({104., 177.0, 123.0})
 {
-    network_ = readNetFromCaffe(FACE_DETECTION_CONFIGURATION, FACE_DETECTION_WEIGHTS);
+    network_ = readNetFromCaffe(FACE_DETECTION_CONFIGURATION, FACE_DETECTION_WEIGHTS);      
     if (network_.empty())
     {
         ostringstream ss;
@@ -20,7 +26,7 @@ FaceDetector::FaceDetector() : confidence_threshold_(0.5), input_image_height_(3
     }
 }
 
-vector<Rect> FaceDetector::detect_face_rectangles(const Mat &frame)
+vector<Rect> FaceDetector::detect_face_rectangles(const Mat &frame)     //converting image into a data blob
 {
     Mat input_blob = blobFromImage(frame, scale_factor_, Size(input_image_width_, input_image_height_), mean_values_, false, false);
     network_.setInput(input_blob, "data");
@@ -28,7 +34,7 @@ vector<Rect> FaceDetector::detect_face_rectangles(const Mat &frame)
     Mat detection_matrix(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>());
 
     vector<Rect> faces;
-    for (int i = 0; i < detection_matrix.rows; i++)
+    for (int i = 0; i < detection_matrix.rows; i++)     //checking for confidence threshold
     {
         float confidence = detection_matrix.at<float>(i, 2);
         if (confidence < confidence_threshold_)
@@ -37,7 +43,7 @@ vector<Rect> FaceDetector::detect_face_rectangles(const Mat &frame)
         int y_left_bottom = static_cast<int>(detection_matrix.at<float>(i, 4) * frame.rows);
         int x_right_top = static_cast<int>(detection_matrix.at<float>(i, 5) * frame.cols);
         int y_right_top = static_cast<int>(detection_matrix.at<float>(i, 6) * frame.rows);
-        faces.emplace_back(x_left_bottom, y_left_bottom, (x_right_top - x_left_bottom), (y_right_top - y_left_bottom));
+        faces.emplace_back(x_left_bottom, y_left_bottom, (x_right_top - x_left_bottom), (y_right_top - y_left_bottom));     //append function
     }
     return faces;
 }
