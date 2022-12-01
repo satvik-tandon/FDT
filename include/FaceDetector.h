3ef48f918a07e906093763c3d33ed95afa7a2b1f
Satvik Tandon <satviktandon@gmail.com>
2022-12-01 14:25:58 +0530

    Update FaceDetector.h

--- include/FaceDetector.h
+++ include/FaceDetector.h
@@ -8,15 +8,15 @@ using namespace dnn;
 class FaceDetector
 {
 public:
-    explicit FaceDetector();
-    vector<Rect> detect_face_rectangles(const Mat &frame);
+    explicit FaceDetector();        // function which detect faces in an image frame
+    vector<Rect> detect_face_rectangles(const Mat &frame);      //a vector containing data of detected faces
 
 private:
     Net network_;
     const int input_image_width_;
     const int input_image_height_;
-    const double scale_factor_;
-    const Scalar mean_values_;
+    const double scale_factor_;     //scale factor when creating image blob. Factor=1 usually.
+    const Scalar mean_values_;      //Mean normalization values network was trained with
     const float confidence_threshold_;
 };
 
